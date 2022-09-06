1a7652193ce7ba58d6dea70c5a358a33f01dd395
Nicholas Padmanabhan <nicholaspad@gmail.com>
2022-09-05 22:00:45 -0400

    Add CAS authentication

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask
 
+from CASClient import CASClient
+
 app = Flask(__name__)
+app.secret_key = b"insecure-key"
 
 
 @app.route("/")
 def hello():
-    return "Hello!"
+    netid = CASClient().authenticate()
+    return netid
