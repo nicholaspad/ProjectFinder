6b651b21a8ac81466b5591fe84b2cfe72d4f9ac5
Nicholas Padmanabhan <nicholaspad@gmail.com>
2022-09-18 16:55:53 -0400

    Format datetimes

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from pytz import timezone
