353a52ad2c26fb1a907b7fea4f4c3add7da306e3
Nicholas Padmanabhan <nicholaspad@gmail.com>
2022-09-18 20:41:25 -0400

    Copy email scripts

---
+++ cron.py
@@ -0,0 +1,85 @@
+from datetime import datetime
+
+import pytz
+
+from emails import *
+from models import *
+from utils import *
+
+BATCH_SIZE = 50
+
+"""
+These two methods are designed to be run once a day at 9am.
+"""
+
+
+def send_overdue_email(test_email=None):
+    header = f"Subject: {OVERDUE_SUBJECT}\n\n"
+    message = header + OVERDUE_MESSAGE
+    to = []
+
+    if test_email:
+        send_email(
+            [test_email],
+            message,
+            EMAIL_PW,
+        )
+        return
+
+    due_date = Config.objects.first().due_date
+    diff = datetime.now(tz=pytz.timezone("US/Eastern")) - due_date
+    if diff.days != 0:
+        return
+
+    users = User.objects.filter(~Q(username="admin"), entry__isnull=True)
+    for user in users:
+        if user.email:
+            to.append(user.email)
+            continue
+        netid = user.username.split("-")[-1]
+        to.append(f"{netid}@princeton.edu")
+
+    for i in range(0, len(to), BATCH_SIZE):
+        send_email(
+            to[i : i + BATCH_SIZE],
+            message,
+            EMAIL_PW,
+        )
+
+    log_email(users, "overdue")
+
+
+def send_reminder_email(test_email=None):
+    header = f"Subject: {REMINDER_SUBJECT}\n\n"
+    message = header + REMINDER_MESSAGE
+    to = []
+
+    if test_email:
+        send_email(
+            [test_email],
+            message,
+            EMAIL_PW,
+        )
+        return
+
+    due_date = Config.objects.first().due_date
+    diff = due_date - datetime.now(tz=pytz.timezone("US/Eastern"))
+    if diff.days != 1:
+        return
+
+    users = User.objects.filter(~Q(username="admin"))
+    for user in users:
+        if user.email:
+            to.append(user.email)
+            continue
+        netid = user.username.split("-")[-1]
+        to.append(f"{netid}@princeton.edu")
+
+    for i in range(0, len(to), BATCH_SIZE):
+        send_email(
+            to[i : i + BATCH_SIZE],
+            message,
+            EMAIL_PW,
+        )
+
+    log_email(users, "reminder")
--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import smtplib
+import ssl
+
 import pytz
 
 from models import *
@@ -8,3 +11,24 @@ def is_past_due():
     return timezone("US/Eastern").localize(config.due_date) < datetime.now(
         tz=pytz.timezone("US/Eastern")
     )
+
+
+def send_email(to_email, message, sender_pw):
+    port = 587
+    smtp_server = "smtp-mail.outlook.com"
+    sender_email = EMAIL
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.starttls(context=context)
+        server.login(sender_email, sender_pw)
+        server.sendmail(sender_email, to_email, message.strip())
+
+
+def log_email(users, email_type):
+    for user in users:
+        EmailLog(
+            user=user,
+            date=datetime.now(tz=pytz.timezone("US/Eastern")),
+            email_type=email_type,
+        ).save()
