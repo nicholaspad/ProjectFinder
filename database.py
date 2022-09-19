f1bccbebab8ea5a3f9ba3a79814c15009bd91f54
Nicholas Padmanabhan <nicholaspad@gmail.com>
2022-09-19 14:17:29 -0400

    Use prod env variable to control db

--- database.py
+++ database.py
@@ -6,9 +6,12 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 load_dotenv()
-DATABASE_URL = os.environ["DATABASE_URL"]
-# must start with postgresql:// instead of postgres://
-engine = create_engine(DATABASE_URL[:8] + "ql" + DATABASE_URL[8:])
+if os.environ["PROD"] == "1":
+    DATABASE_URL = os.environ["DATABASE_URL"]
+    # must start with postgresql:// instead of postgres://
+    engine = create_engine(DATABASE_URL[:8] + "ql" + DATABASE_URL[8:])
+else:
+    engine = create_engine("sqlite:///test.db")
 db = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 Base = declarative_base()
 Base.query = db.query_property()
