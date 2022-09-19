6eaa9792cddda872e2a1a4c74b384fbab96837ac
Nicholas Padmanabhan <nicholaspad@gmail.com>
2022-09-19 11:38:01 -0400

    Use Postgres database URL

--- database.py
+++ database.py
@@ -1,8 +1,14 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine("sqlite:///test.sqlite")
+load_dotenv()
+DATABASE_URL = os.environ["DATABASE_URL"]
+# must start with postgresql:// instead of postgres://
+engine = create_engine(DATABASE_URL[:8] + "ql" + DATABASE_URL[8:])
 db = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 Base = declarative_base()
 Base.query = db.query_property()
